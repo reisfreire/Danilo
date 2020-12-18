c434221d7c73ae510165562f28feaad6f9a58fbe
@reisfreire <reisfreire@globo.com>
2020-12-18 12:24:21 -0300

    Commit Banco de Dadoos

---
+++ Curso C#/Tres_Camadas/Database/Pessoa.cs
@@ -0,0 +1,81 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Database
+{
+    public class Pessoa : IPessoa
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string CPF { get; set; }
+
+        public virtual void Gravar()
+        {
+            var dados = this.Ler();
+            dados.Add(this);
+            // if (File.Exists(diretorioComArquivo()))
+            //{
+            // Em verde era a primeira solução usando o \n no fim da string
+            //    string conteudo = "nome;telefone;cpf;\n";
+            string conteudo = "nome;telefone;cpf;";
+            // Em destaque a nova solução usando O streamWriter que grava sempre numa nova linha;
+            // o streamWriter se não existir o arquivo, ele o cria
+            StreamWriter r = new StreamWriter(diretorioComArquivo());
+            r.WriteLine(conteudo);
+
+            foreach (Pessoa b in dados)
+            {
+                // conteudo += cliente.Nome + ";" + cliente.Telefone + ";" + cliente.CPF + ";\n";
+                var linha = b.Nome + ";" + b.Telefone + ";" + b.CPF + ";";
+                r.WriteLine(linha);
+            }
+            r.Close();
+            // File.WriteAllText(caminhoBaseClientes(), conteudo);
+            //}
+        }
+        public virtual List<Pessoa> Ler()  /// Ele colocou o tipo como IPessoa
+        {
+            var dados = new List<Pessoa>();
+            if (File.Exists(diretorioComArquivo()))
+            {
+                {
+
+                    using (StreamReader arquivo = File.OpenText(diretorioComArquivo()))
+                    {
+                        string linha;
+                        int i = 0;
+                        while ((linha = arquivo.ReadLine()) != null)
+                        {
+                            i++;
+                            if (i == 1) continue;
+                            var baseArquivo = linha.Split(';');
+                            /*var cliente = new Cliente();
+                            cliente.Nome = clienteArquivo[0];
+                            cliente.Telefone = clienteArquivo[1];
+                            cliente.CPF= clienteArquivo[2];
+                            */
+                            //  Cria uma instäncia em runtime de qualquer tipo
+                            var dado = (Pessoa)Activator.CreateInstance(this.GetType()); //IPessoa em vez de Pessoa
+                            dado.Nome=(baseArquivo[0]);
+                            dado.Telefone=(baseArquivo[1]);
+                            dado.CPF=(baseArquivo[2]);
+                            dados.Add(dado);
+                        }
+                    }
+                }
+            }
+            return dados;
+        }
+        internal string diretorioComArquivo()
+        {
+            return ConfigurationManager.AppSettings["CaminhoArquivos"] + this.GetType().Name + ".txt";
+        }
+    }
+}
---
+++ Curso C#/Tres_Camadas/UI/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGravar = new System.Windows.Forms.Button();
+            this.maskedTextBoxCPF = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBoxTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGravar
+            // 
+            this.buttonGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGravar.Location = new System.Drawing.Point(67, 372);
+            this.buttonGravar.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonGravar.Name = "buttonGravar";
+            this.buttonGravar.Size = new System.Drawing.Size(138, 72);
+            this.buttonGravar.TabIndex = 24;
+            this.buttonGravar.Text = "Gravar";
+            this.buttonGravar.UseVisualStyleBackColor = true;
+            this.buttonGravar.Click += new System.EventHandler(this.buttonGravar_Click);
+            // 
+            // maskedTextBoxCPF
+            // 
+            this.maskedTextBoxCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxCPF.Location = new System.Drawing.Point(349, 239);
+            this.maskedTextBoxCPF.Margin = new System.Windows.Forms.Padding(6);
+            this.maskedTextBoxCPF.Mask = "000,000,000,00";
+            this.maskedTextBoxCPF.Name = "maskedTextBoxCPF";
+            this.maskedTextBoxCPF.Size = new System.Drawing.Size(272, 29);
+            this.maskedTextBoxCPF.TabIndex = 23;
+            // 
+            // maskedTextBoxTelefone
+            // 
+            this.maskedTextBoxTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxTelefone.Location = new System.Drawing.Point(349, 174);
+            this.maskedTextBoxTelefone.Margin = new System.Windows.Forms.Padding(6);
+            this.maskedTextBoxTelefone.Mask = "(99) 0000-0000";
+            this.maskedTextBoxTelefone.Name = "maskedTextBoxTelefone";
+            this.maskedTextBoxTelefone.Size = new System.Drawing.Size(272, 29);
+            this.maskedTextBoxTelefone.TabIndex = 22;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(349, 103);
+            this.textBoxNome.Margin = new System.Windows.Forms.Padding(6);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(349, 29);
+            this.textBoxNome.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 66);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(342, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Pode allterar a fonte dos 3 controles juntos basta selecionar os 3 juntos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 244);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 24);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "CPF:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Telefone: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 108);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nome: ";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(765, 24);
+            this.menuStrip1.TabIndex = 25;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 544);
+            this.Controls.Add(this.buttonGravar);
+            this.Controls.Add(this.maskedTextBoxCPF);
+            this.Controls.Add(this.maskedTextBoxTelefone);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGravar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxCPF;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTelefone;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
